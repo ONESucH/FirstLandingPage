4d4d11cc55290d2191fd7f173e593dc65494f674
Onesuch <experts1994@mail.ru>
2016-05-23 10:40:39 +0300

    fix

--- js/common.js
+++ js/common.js
@@ -1,6 +1,31 @@
-$(document).ready( function () {
-   $(".topMenuOne").mPageScroll2id({
-        offset: 50,
-        scrollSpeed: 500
-    })
-});
+//var allTask = {
+//    topMenuOne : document.getElementById('#topMenuOne'),
+//    MenuTo : document.getElementById('#MenuTo'),
+//    MenuThree : document.getElementById('#MenuThree')
+//};
+//
+//allTask.topMenuOne.addEventListener('click', scroll);
+//
+//function scroll() {
+//    if (allTask.topMenuOne === true) {
+//        allTask.topMenuOne.scrollToBySpeed({
+//            speed: 500,
+//            offset: -100
+//        });
+//        console.log('Первый скрол активирован', allTask.topMenuOne);
+//    }
+//}
+
+
+//$('#topMenuOne').scrollToBySpeed({
+//    speed: 500,
+//    offset: -100
+//});
+//$('#MenuTo').scrollToBySpeed({
+//    speed: 500,
+//    offset: -100
+//});
+//$('#MenuThree').scrollToBySpeed({
+//    speed: 500,
+//    offset: -100
+//});
--- js/main.js
+++ js/main.js
@@ -1,12 +1,17 @@
+var visible = false;
 var allTask = {
     faBars : document.getElementById('faBars'),
     menuToggle : document.getElementById('menuToggle'),
-    middleMainContainer : document.getElementById('middleMainContainer')
+    middleMainContainer : document.getElementById('middleMainContainer'),
+    topMenuOne : document.getElementById('#topMenuOne'),
+    MenuTo : document.getElementById('#MenuTo'),
+    MenuThree : document.getElementById('#MenuThree'),
+    navRight : document.getElementById('#navRight')
 };
 
 allTask.faBars.addEventListener('click', showFun);
+allTask.addEventListener('click', scroll);
 
-var visible = false;
 function showFun() {
     if (visible) {
         allTask.menuToggle.style.display = 'none';
@@ -15,4 +20,11 @@ function showFun() {
         allTask.menuToggle.style.display = 'block';
         visible = true;
     }
+}
+
+function scroll() {
+    allTask.scrollToBySpeed({
+        speed: 500,
+        offset: -100
+    });
 }
